91eb51b2515c522e1d9089c257bfe63254ace197
javirrof <javirofe96@gmail.com>
2024-10-29 19:16:28 +0100

    Add check to avoid save duplicated information in db

--- src/db/manageDbData.ts
+++ src/db/manageDbData.ts
@@ -1,8 +1,14 @@
-import mongoose from "mongoose"
-import { nftMetadataSchema } from "./schemas"
+import mongoose from 'mongoose'
+import { nftMetadataSchema } from './schemas'
 
 export const addNftMetadata = async (name: string, description: string, image: string) => {
   const NftMetadata = mongoose.model('NftMetadata', nftMetadataSchema)
+
+  const existingMetadata = await NftMetadata.findOne({ name, description, image })
+  if (existingMetadata) {
+    console.log('Metadata already exists. Do not save again.')
+    return
+  }
   const nftMetadata = new NftMetadata({ name, description, image })
   await nftMetadata.save()
 }
